b64f0049f1b74962282ca8b28facdea78e518f74
Rafal Krzyzanowski <rkr1410@gmail.com>
2021-09-22 21:10:46 +0200

    Test refactor (extract source methods)
    Moved test source methods to a separate class file

--- app/src/test/kotlin/net/rkr1410/yaklox/ScannerTest.kt
+++ app/src/test/kotlin/net/rkr1410/yaklox/ScannerTest.kt
@@ -6,92 +6,12 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.lang.IllegalStateException
 
 internal class ScannerTest {
-    companion object {
-        @JvmStatic
-        fun singleTokenSource() = listOf(
-            Arguments.of("(", LEFT_PAREN),
-            Arguments.of(")", RIGHT_PAREN),
-            Arguments.of("{", LEFT_BRACE),
-            Arguments.of("}", RIGHT_BRACE),
-            Arguments.of(",", COMMA),
-            Arguments.of(".", DOT),
-            Arguments.of("-", MINUS),
-            Arguments.of("+", PLUS),
-            Arguments.of(";", SEMICOLON),
-            Arguments.of("*", STAR),
-            Arguments.of("!", BANG),
-            Arguments.of("/", SLASH),
-            Arguments.of("=", EQUAL),
-            Arguments.of("<", LESS),
-            Arguments.of(">", GREATER),
-            Arguments.of("==", EQUAL_EQUAL),
-            Arguments.of("!=", BANG_EQUAL),
-            Arguments.of("<=", LESS_EQUAL),
-            Arguments.of(">=", GREATER_EQUAL),
-        )
-
-        @JvmStatic
-        fun numberSource() = listOf(
-            Arguments.of("0", 0.toDouble(), "Zero"),
-            Arguments.of("0.0", 0.0, "Zero with zero decimal part"),
-            Arguments.of("0.222", 0.222, "Zero with decimal part"),
-            Arguments.of("1234", 1234.toDouble(), "No decimal part number"),
-            Arguments.of("1234.997", 1234.997, "Number with decimal part"),
-        )
-
-        @JvmStatic
-        fun keywordSource() = listOf(
-            Arguments.of("and", AND),
-            Arguments.of("class", CLASS),
-            Arguments.of("else", ELSE),
-            Arguments.of("false", FALSE),
-            Arguments.of("for", FOR),
-            Arguments.of("fun", FUN),
-            Arguments.of("if", IF),
-            Arguments.of("nil", NIL),
-            Arguments.of("or", OR),
-            Arguments.of("return", RETURN),
-            Arguments.of("super", SUPER),
-            Arguments.of("this", THIS),
-            Arguments.of("true", TRUE),
-            Arguments.of("var", VAR),
-            Arguments.of("while", WHILE),
-        )
-
-        @JvmStatic
-        fun identifierSource() = listOf(
-            Arguments.of("variableName"),
-            Arguments.of("varName"),
-            Arguments.of("v"),
-            Arguments.of("v_"),
-            Arguments.of("_v"),
-            Arguments.of("_v_"),
-            Arguments.of("v1"),
-            Arguments.of("v_1"),
-            Arguments.of("v191something_02y"),
-            Arguments.of("reallySeriouslyAbsurdlyAndDefinitivelyVeryLongActuallyJustAboutTooLongIfIAmToBe" +
-                    "CompletelyFrankWithYouAlthoughTooLongAsInByMeasureOfWhatMakesSenseForAHumanReadableVariableName" +
-                    "AndNotTooLongAsInMakingTheCompilationFailAsInFactThisShouldPassCompilationVariableName"),
-        )
-
-        @JvmStatic
-        fun stringSource() = listOf(
-            Arguments.of("\"\"", "Empty string"),
-            Arguments.of("\"b\"", "Single letter string"),
-            Arguments.of("\"this\"", "Single word string"),
-            Arguments.of("\"this is a test string\"", "Multiple words string"),
-            Arguments.of("\"this is a test string\n\"", "Two-line string"),
-            Arguments.of("\"this is a test string\nThe second line\nThird line\"", "Multiple-line string"),
-        )
-    }
-
     @ParameterizedTest(name = "{0} produces a NUMBER token")
-    @MethodSource("numberSource")
+    @MethodSource("net.rkr1410.yaklox.ScannerTestSources#numberSource")
     fun testNumberTokens(codeAndLexeme: String, literal: Double) {
         assertScannerFor(codeAndLexeme)
             .producesFirstTokenOfType(NUMBER)
@@ -102,7 +22,7 @@ internal class ScannerTest {
     }
 
     @ParameterizedTest(name = "{0} produces a {1} token")
-    @MethodSource("singleTokenSource")
+    @MethodSource("net.rkr1410.yaklox.ScannerTestSources#singleTokenSource")
     fun testSingleTokens(codeAndLexeme: String, tokenType: TokenType) {
         assertScannerFor(codeAndLexeme)
             .producesFirstTokenOfType(tokenType)
@@ -113,7 +33,7 @@ internal class ScannerTest {
     }
 
     @ParameterizedTest(name = "{0} produces a {1} keyword token")
-    @MethodSource("keywordSource")
+    @MethodSource("net.rkr1410.yaklox.ScannerTestSources#keywordSource")
     fun testKeywordTokens(codeAndLexeme: String, tokenType: TokenType) {
         assertScannerFor(codeAndLexeme)
             .producesFirstTokenOfType(tokenType)
@@ -124,7 +44,7 @@ internal class ScannerTest {
     }
 
     @ParameterizedTest(name = "{0} produces an IDENTIFIER token")
-    @MethodSource("identifierSource")
+    @MethodSource("net.rkr1410.yaklox.ScannerTestSources#identifierSource")
     fun testIdentifierTokens(codeAndLexeme: String) {
         assertScannerFor(codeAndLexeme)
             .producesFirstTokenOfType(IDENTIFIER)
@@ -135,7 +55,7 @@ internal class ScannerTest {
     }
 
     @ParameterizedTest(name = "String test: {1}")
-    @MethodSource("stringSource")
+    @MethodSource("net.rkr1410.yaklox.ScannerTestSources#stringSource")
     fun testValidString(stringValue: String, desc: String) {
         val newLIneCount = stringValue.count { it == '\n' } + 1
         assertScannerFor(stringValue)
@@ -217,13 +137,6 @@ internal class ScannerTest {
         }
     }
 
-    // Just test some code with random variations of symbols and operations allowed.
-    // Also, check the last parsed token is a EOF, and it's line corresponds to last
-    // line number in the file. This should more-or-less mean all tokens scanned
-    // successfully. A nonspecific test at best, but maybe it's going to catch some
-    // mistake one day? Who knows.
-    // Anyway, it *is* a requirement on the app, and as such deserves its own
-    // test, even if the net might be cast a bit too wide.
     @Test
     @DisplayName("Regression scan: a file with some code has EOF token on the last file line")
     fun testRandomScript() {
---
+++ app/src/test/kotlin/net/rkr1410/yaklox/ScannerTestSources.kt
@@ -0,0 +1,86 @@
+package net.rkr1410.yaklox
+
+import org.junit.jupiter.params.provider.Arguments
+import net.rkr1410.yaklox.TokenType.*
+
+class ScannerTestSources {
+    companion object {
+        // lexemes and tokens they are expected to produce
+        @JvmStatic
+        fun singleTokenSource() = listOf(
+            Arguments.of("(", LEFT_PAREN),
+            Arguments.of(")", RIGHT_PAREN),
+            Arguments.of("{", LEFT_BRACE),
+            Arguments.of("}", RIGHT_BRACE),
+            Arguments.of(",", COMMA),
+            Arguments.of(".", DOT),
+            Arguments.of("-", MINUS),
+            Arguments.of("+", PLUS),
+            Arguments.of(";", SEMICOLON),
+            Arguments.of("*", STAR),
+            Arguments.of("!", BANG),
+            Arguments.of("/", SLASH),
+            Arguments.of("=", EQUAL),
+            Arguments.of("<", LESS),
+            Arguments.of(">", GREATER),
+            Arguments.of("==", EQUAL_EQUAL),
+            Arguments.of("!=", BANG_EQUAL),
+            Arguments.of("<=", LESS_EQUAL),
+            Arguments.of(">=", GREATER_EQUAL),
+        )
+
+        @JvmStatic
+        fun numberSource() = listOf(
+            Arguments.of("0", 0.toDouble(), "Zero"),
+            Arguments.of("0.0", 0.0, "Zero with zero decimal part"),
+            Arguments.of("0.222", 0.222, "Zero with decimal part"),
+            Arguments.of("1234", 1234.toDouble(), "No decimal part number"),
+            Arguments.of("1234.997", 1234.997, "Number with decimal part"),
+        )
+
+        @JvmStatic
+        fun keywordSource() = listOf(
+            Arguments.of("and", AND),
+            Arguments.of("class", CLASS),
+            Arguments.of("else", ELSE),
+            Arguments.of("false", FALSE),
+            Arguments.of("for", FOR),
+            Arguments.of("fun", FUN),
+            Arguments.of("if", IF),
+            Arguments.of("nil", NIL),
+            Arguments.of("or", OR),
+            Arguments.of("return", RETURN),
+            Arguments.of("super", SUPER),
+            Arguments.of("this", THIS),
+            Arguments.of("true", TRUE),
+            Arguments.of("var", VAR),
+            Arguments.of("while", WHILE),
+        )
+
+        @JvmStatic
+        fun identifierSource() = listOf(
+            Arguments.of("variableName"),
+            Arguments.of("varName"),
+            Arguments.of("v"),
+            Arguments.of("v_"),
+            Arguments.of("_v"),
+            Arguments.of("_v_"),
+            Arguments.of("v1"),
+            Arguments.of("v_1"),
+            Arguments.of("v191something_02y"),
+            Arguments.of("reallySeriouslyAbsurdlyAndDefinitivelyVeryLongActuallyJustAboutTooLongIfIAmToBe" +
+                    "CompletelyFrankWithYouAlthoughTooLongAsInByMeasureOfWhatMakesSenseForAHumanReadableVariableName" +
+                    "AndNotTooLongAsInMakingTheCompilationFailAsInFactThisShouldPassCompilationVariableName"),
+        )
+
+        @JvmStatic
+        fun stringSource() = listOf(
+            Arguments.of("\"\"", "Empty string"),
+            Arguments.of("\"b\"", "Single letter string"),
+            Arguments.of("\"this\"", "Single word string"),
+            Arguments.of("\"this is a test string\"", "Multiple words string"),
+            Arguments.of("\"this is a test string\n\"", "Two-line string"),
+            Arguments.of("\"this is a test string\nThe second line\nThird line\"", "Multiple-line string"),
+        )
+    }
+}
